f56631b8ba70b6ee5f77b390c90662314c022f45
mblancosoto <mblancosoto@nautilus30.danielcastelao.org>
2017-05-15 09:13:28 +0200

    commit5

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -2,17 +2,35 @@
 package reloj;
 
 import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JOptionPane;
+
 
 public class Reloj {
 
     static LocalTime horaActual;
     static LocalTime horaAlarma;
+    static Timer timer ;
+    static TimerTask task;
+     
     
     public static void main(String[] args) {
-    horaActual = LocalTime.of(8,55);
+    task = new TimerTask() {
+        @Override
+        public void run() {
+          if(horaAlarma.getMinute()==LocalTime.now().getMinute()){
+              JOptionPane.showMessageDialog(null,"ALARMA!!!");
+          }else{
+              JOptionPane.showMessageDialog(null,"La hora Actual es : " +LocalTime.now().getHour()+":"+LocalTime.now().getMinute());
+          }
+        }
+    };
+    timer = new Timer();
+    horaActual = LocalTime.now();
     horaAlarma = LocalTime.of(3, 15);
     int select;
+    timer.schedule(task,10, 10000);
     do{
         select = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduzca la opción Deseada \n 1. configurar Hora  \n 2. hora + "
                 + "\n 3.minutos +"));
@@ -29,12 +47,14 @@ public class Reloj {
          break;
          
          
+         
      }   
         
     }while(select!=0);
-    
+    timer.cancel();
     
     }
+
     
     
 }
